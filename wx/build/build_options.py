8da17c2b57d205ff57ced370c71b1c9a5661f79e
Gautam Daryanani <gdaryanani@Gautams-MacBook-Pro.local>
2016-10-24 00:20:13 -0700

    adding wx package

---
+++ wx/build/build_options.py
@@ -0,0 +1,10 @@
+
+UNICODE=1
+UNDEF_NDEBUG=1
+INSTALL_MULTIVERSION=0
+FLAVOUR=""
+EP_ADD_OPTS=1
+EP_FULL_VER=0
+WX_CONFIG="/usr/local/bin/wx-config --prefix=/usr/local"
+WXPORT="osx_cocoa"
+MONOLITHIC=0
--- wxtest.py
+++ wxtest.py
@@ -1,5 +1,5 @@
 import sys
-import wxversion
+import wx
  
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
 frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
